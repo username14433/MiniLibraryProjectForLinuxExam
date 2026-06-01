062853a0861cef203799c6308bbf5e0145bc079f
Rockend <90926372+username14433@users.noreply.github.com>
2026-06-02 02:03:45 +0300

    Change server URL port from 5000 to 5050

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using LinuxAdminExamApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseUrls("http://0.0.0.0:5000");
+builder.WebHost.UseUrls("http://0.0.0.0:5050");
 
 builder.Services.AddControllers();
 builder.Services.AddSingleton<InMemoryStore>();
